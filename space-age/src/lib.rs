362f0caef7c485f530987b41e246122ae5352daf
huchanghao <2955568032@qq.com>
2023-10-05 00:24:00 +0800

    use macro to refactory

--- space-age/src/lib.rs
+++ space-age/src/lib.rs
@@ -11,58 +11,26 @@ impl From<u64> for Duration {
 }
 
 pub trait Planet {
-    fn get_orbital_period()->f64;
-    fn years_during(d: &Duration) -> f64 {
-        d.0 / Self::get_orbital_period()
-    }
+    fn years_during(d: &Duration) -> f64;
 }
 
-pub struct Mercury;
-pub struct Venus;
-pub struct Earth;
-pub struct Mars;
-pub struct Jupiter;
-pub struct Saturn;
-pub struct Uranus;
-pub struct Neptune;
 
-impl Planet for Mercury {
-    fn get_orbital_period()->f64 {
-        0.2408467
-    }
+macro_rules! planet {
+    ($planet:ident, $period:expr) => {
+        pub struct $planet;
+        impl Planet for $planet {
+            fn years_during(d: &Duration) -> f64 {
+                d.0 / $period
+            }
+        }
+    };
 }
-impl Planet for Venus {
-    fn get_orbital_period()->f64 {
-        0.61519726
-    }
-}
-impl Planet for Earth {
-    fn get_orbital_period()->f64 {
-        1.0
-    }
-}
-impl Planet for Mars {
-    fn get_orbital_period()->f64 {
-        1.8808158
-    }
-}
-impl Planet for Jupiter {
-    fn get_orbital_period()->f64 {
-        11.862615
-    }
-}
-impl Planet for Saturn {
-    fn get_orbital_period()->f64 {
-        29.447498
-    }
-}
-impl Planet for Uranus {
-    fn get_orbital_period()->f64 {
-        84.016846
-    }
-}
-impl Planet for Neptune {
-    fn get_orbital_period()->f64 {
-        164.79132
-    }
-}
+
+planet!(Mercury,0.2408467);
+planet!(Venus,0.61519726);
+planet!(Earth,1.0);
+planet!(Mars,1.8808158);
+planet!(Jupiter,11.862615);
+planet!(Saturn,29.447498);
+planet!(Uranus,84.016846);
+planet!(Neptune,164.79132);
