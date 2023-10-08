5adba2e64480d8a7ec5bccc587255253e206f2fa
huchanghao <2955568032@qq.com>
2023-10-08 19:55:58 +0800

    finish sublist

--- sublist/src/lib.rs
+++ sublist/src/lib.rs
@@ -7,5 +7,21 @@ pub enum Comparison {
 }
 
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    todo!("Determine if the first list is equal to, sublist of, superlist of or unequal to the second list.");
+    if _first_list == _second_list {
+        return Comparison::Equal;
+    }
+    let mut shorter = _first_list;
+    let mut longer = _second_list;
+    let mut sub_list =  Comparison::Sublist;
+
+    if _first_list.len() > _second_list.len() {
+        (shorter,longer) = (longer, shorter);
+        sub_list = Comparison::Superlist;
+    }
+
+    if shorter.is_empty() || longer.windows(shorter.len()).any(|window| window == shorter) {
+        sub_list
+    }else {
+         Comparison::Unequal
+    }
 }
